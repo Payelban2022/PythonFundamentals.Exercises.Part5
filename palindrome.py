493e3f3815159f3d6cf34130973397dcf7dccb5d
payel <payel.nandy2580@gmail.com>
2022-07-10 17:16:17 -0400

    Palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,13 @@
-def is_palindrome(value: str) -> bool:
+def is_palindrome(self) -> bool:
     """
     This function determines if a word or phrase is a palindrome
 
     :param value: A string
     :return: A boolean
     """
-    pass  # remove pass statement and implement me
+    # pass  # remove pass statement and implement me
+
+    if self[::1] == self[::-1]:
+        return True
+    else:
+        return False
