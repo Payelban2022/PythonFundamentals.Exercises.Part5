4ff407bba2c7dce2cb2b2e3314ca64749483586f
payel <payel.nandy2580@gmail.com>
2022-07-10 17:15:46 -0400

    Anagram

--- anagram.py
+++ anagram.py
@@ -1,6 +1,10 @@
-def is_anagram(first_string: str, second_string: str) -> bool:
+def is_anagram(str1, str2) -> bool:
     """
     Given two strings, this functions determines if they are an anagram of one another.
     """
-    pass  # remove pass statement and implement me
+    # pass  # remove pass statement and implement me
+    if(sorted(str1) == sorted(str2)):
+        return True
+    else:
+        return False
 
