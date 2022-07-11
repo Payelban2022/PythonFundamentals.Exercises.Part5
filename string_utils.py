8a959b7a43439b837ec27013952c454d28b10313
payel <payel.nandy2580@gmail.com>
2022-07-11 13:53:41 -0400

    String utilities

--- string_utils.py
+++ string_utils.py
@@ -1,33 +1,60 @@
 
-def str_len(str_in: str) -> str:
+def str_len(word) -> int:
+
     """
     Given a string parameter, this function should return the length of the parameter.
     """
-    pass  # remove pass statement and implement me
+    # pass  # remove pass statement and implement me
+#     a = input("Please input the word")
+#     return a
+# def word(name_word):
+#     print(len(word), ",", word)
+    length = (len(word))
+    return length
+
+print(str_len("word"))
 
 
-def first_char(str_in: str) -> str:
+def first_char(word) -> str:
     """
     Given a string parameter, this function should return the first letter of the parameter.
     """
-    pass  # remove pass statement and implement me
+    # pass  # remove pass statement and implement me
+#     print("The first character is:", str1[:1])
+# str1 = ("Welcome to String length.")
+# first_char(str1)
+    firstchar = word[0]
+    return firstchar
+print (first_char("good"))
 
 
-def last_char(str_in: str) -> str:
+
+
+
+
+def last_char(word) -> str:
     """
     Given a string parameter, this function should return the last letter of the parameter..
     """
-    pass  # remove pass statement and implement me
+    # pass  # remove pass statement and implement me
+    lastchar = word[-1]
+    return lastchar
+print(last_char("good"))
 
 
-def input_has_substring(str_in: str, sub_str_in: str) -> bool:
+def input_has_substring(fullstring,substring) -> bool:
     """
     This function determines if the substring exists within the string. Returns True or False.
     """
     pass  # remove pass statement and implement me
+    if substring in fullstring:
+        return True
+    else:
+        return False
 
 
-def substring(str_in: str, start: int, stop: int) -> str:
+
+def substring(s, start, stop) -> str:
     """
     Returns the substring of a string.
 
@@ -36,13 +63,17 @@ def substring(str_in: str, start: int, stop: int) -> str:
     start -- starting position of the input parameter to start the substring (inclusive)
     stop -- stopping position of the input parameter to stop the substring (exclusive)
     """
-    pass  # remove pass statement and implement me
+    # pass  # remove pass statement and implement me
+    return s[start:stop]
+print(substring("beautiful",2,5))
 
-
-def opposite_case(str_in: str) -> str:
+def opposite_case(word) -> str:
     """
     Given a string parameter, this function returns the same string back with each letter having the opposite case.
     Example: 
     When input = "Python" the function returns "pYTHON"
     """
-    pass  # remove pass statement and implement me
+    # pass  # remove pass statement and implement me
+    opposite = word.swapcase()
+    return opposite
+print(opposite_case("Python"))
